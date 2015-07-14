693f5d010c34fe614313061c44e34abab2305718
KaiyuanZhao <dogin2006@hotmail.com>
2015-07-14 16:43:38 +0800

    Complete all the function in OrderAction.php

--- action/OrderAction.php
+++ action/OrderAction.php
@@ -9,6 +9,9 @@
  */
 class OrderAction
 {
+    public static $ORDER_MEAL_FAIL = -1;
+    public static $CANCEL_ORDER_NOT_ORDER_BEFOR = -2;
+    public static $GET_ORDERS_FAIL = -3;
 
     /**
      * @param $userId Integer
@@ -16,15 +19,34 @@ class OrderAction
      */
     public static function orderMeal($userId)
     {
+        $connection = Database::getInstance()->getConnection();
+
+        static $query = "insert into `order` (userId, `date`) values (?, ?)";
+        $result = $connection->prepare($query);
+        $date = TimeUtils::getCurrentDate();
+        $result->bind_param("ss", $userId, $date);
+        $execute = $result->execute();
+        if (!$execute)
+            return self::$ORDER_MEAL_FAIL;
+        $result->close();
         return true;
     }
 
     /**
-     * @param $userId String
+     * @param $userId Integer
      * @return bool|Integer cancel success or error code
      */
     public static function cancelOrder($userId)
     {
+        $connection = Database::getInstance()->getConnection();
+
+        static $query = "delete from `order` where userid = ?";
+        $result = $connection->prepare($query);
+        $result->bind_param("s", $userId);
+        $execute = $result->execute();
+        if (!$execute)
+            return self::$CANCEL_ORDER_NOT_ORDER_BEFOR;
+        $result->close();
         return true;
     }
 
@@ -32,9 +54,25 @@ class OrderAction
      * @param $date String
      * @return array|Integer get orders success or error code
      */
-    public static function getOrders($date)
+    public static function getOrders($date = "")
     {
-        return array(new Order(0, new User(0, "zhaokaiyuan@baixing.net", "赵开元", "Kyle", "暑期实习生", "1806", ""), new DateTime(), $date));
+        if ($date == "")
+            $date = TimeUtils::getCurrentDate();
+        $connection = Database::getInstance()->getConnection();
+        static $query = "select * from `order` natural join user where date = ? order by createTime desc";
+        $result = $connection->prepare($query);
+        $result->bind_param("s", $date);
+        $execute = $result->execute();
+        if (!$execute)
+            return self::$GET_ORDERS_FAIL;
+        $result->bind_result($userId, $orderId, $date, $createTime, $email, $password, $username, $nickname,
+            $department, $location, $description);
+        $orders = [];
+        while ($result->fetch())
+            $orders[] = new Order($orderId, new User($userId, $email, $username, $nickname, $department,
+                $location, $description), $date, $createTime);
+        $result->close();
+        return $orders;
     }
 
 }

77eaf9de3250a4ab332dcd3766a10f36a51f3c6f
EdwardChor <13307130149@fudan.edu.cn>
2015-07-15 10:24:08 +0800

    修正了一些post的使用

---
+++ ftest.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: EdwardChor
+ * Date: 7/15/15
+ * Time: 10:17 AM
+ */
+
+$file=$_FILES["fd"];
+if($file!=null){
+    echo "done!";
+}
+else{
+    echo "error!";
+
+}
+?>
--- ltest.php
+++ ltest.php
@@ -1,3 +1,6 @@
 <?php
+	echo $_POST["username"];
+	echo $_POST["password"];
 	echo "success!";
+
 ?>

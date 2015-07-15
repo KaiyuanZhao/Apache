1d063b1ad1fc55ee5ed721413a0962b3c225bbf2
lyn <tongji_lyn@163.com>
2015-07-15 11:01:41 +0800

    login and register finished    return json (success: true of false  errormessage:)

--- submit/submit.php
+++ submit/submit.php
@@ -16,42 +16,54 @@
     require_once "../entity/response/Response.php";
     session_start();
     $arr = $_POST;
-    $result = testLogin($arr);
+    $result = testReg($arr);
     $myjson = my_json_encode($result);
     echo $myjson;
 
-    
-    $useraction=new UserAction();
-    var_dump($_POST);
-    $checkFormat=true;
-    $email=($_POST["email"]);
-    $psw=($_POST["psw"]);
-    $username=($_POST["username"]);
-    $nickname=($_POST["nickname"]);
-    $department=($_POST["department"]);
-    $location=($_POST["location"]);
-    $description=($_POST["description"]);
-    //echo  $user->getEmail();
 
-    $testformat=new testFormat();
-    if (!$testformat->testReg($email,$psw,$username,$nickname,$department,$location,$description)) {
-        $checkFormat = false;
-        echo "wrong format";
-    }
-//一系列表格验证！暂略
+  //  var_dump($_POST);
+    function testReg($arr)
+    {
+        $checkFormat = true;
+        $email = ($_POST["email"]);
+        $psw = ($_POST["psw"]);
+        $username = ($_POST["username"]);
+        $nickname = ($_POST["nickname"]);
+        $department = ($_POST["department"]);
+        $location = ($_POST["location"]);
+        $description = ($_POST["description"]);
+        $icon = "ss";
+        //echo  $user->getEmail();
+
+        $testformat = new testFormat();
+        if (!$testformat->testReg($email, $psw, $username, $nickname, $department, $location, $description)) {
+            $checkFormat = false;
+           // echo "wrong format";
+            $result=new Response(false,"wrong format");
+            return $result;
+        }
+
 // echo  $username=$_POST["username"
 
-    if ($checkFormat) {
-        $user = $useraction->register($email, $psw, $username, $nickname, $department, $location, $description，$icon="");
-        if ($user == -1)
-            echo "register fail";
-        elseif ($user == -2)
-            echo "email has been registered";
-        elseif (isset($user)) {
-            $_SESSION['user'] = $user;
-            header("Location: ../index/userc.php");
+        if ($checkFormat) {
+            $user = UserAction::register($email, $psw, $username, $nickname, $department, $location, $description, $icon);
+            if ($user === -1){
+                $result=new Response(false,"register fail");
+            }
+            elseif ($user === -2){
+                $result=new Response(false,"email has been registerd");
+                return $result;
+            }
+            elseif (isset($user)) {
+                $_SESSION['user'] = $user;
+                $result=new Response(true);
+                return $result;
+            }
         }
     }
 
-
+function my_json_encode($phparr)
+{
+    return json_encode($phparr);
+}
 ?>

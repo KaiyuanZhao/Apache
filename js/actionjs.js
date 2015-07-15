99c578631107fca12ebc9ec9ff103207a4d08fb1
EdwardChor <13307130149@fudan.edu.cn>
2015-07-15 13:45:41 +0800

    修正了submit.php的文件名

--- js/actionjs.js
+++ js/actionjs.js
@@ -5,7 +5,7 @@ function login_submit(){
     lemail=$("#lemail").val();
     lpassword=$("#lpassword").val();
 
-    $.post("ltest.php",{username:lemail,password:lpassword},function(data,status){
+    $.post("submit/submit.php",{username:lemail,password:lpassword},function(data,status){
         alert("Data: " + data + "\nStatus: " + status);
     });
 }
@@ -19,10 +19,10 @@ function signup_submit() {
     staste = $("#staste").val();
 
 
-    /* $.post("stest.php",{email:semail,username:susername,taste:staste,password:spassword,location:slocation},function(data,status){
+    $.post("stest.php",{email:semail,username:susername,taste:staste,password:spassword,location:slocation},function(data,status){
      alert("Data: " + data + "\nStatus: " + status);
      });
-     */
+
 
         alert("test");
         $.ajaxFileUpload
@@ -43,34 +43,5 @@ function signup_submit() {
             }
         );
 
-
-/*
-    $.fn.AjaxFileUpload
-    (
-        {
-            url: 'ftest.php', //用于文件上传的服务器端请求地址
-            secureuri: false, //是否需要安全协议，一般设置为false
-            fileElementId: 'savator', //文件上传域的ID
-            dataType: 'json', //返回值类型 一般设置为json
-            success: function (data, status)  //服务器成功响应处理函数
-            {
-                alert(status);
-                //alert(data);
-                //$("#img1").attr("src", data.imgurl);
-                //if (typeof (data.error) != 'undefined') {
-                //    if (data.error != '') {
-                //        alert(data.error);
-                //    } else {
-                //        alert(data.msg);
-                //    }
-                //}
-            },
-            error: function (data, status, e)//服务器响应失败处理函数
-            {
-                alert(e);
-            }
-        }
-    );
-*/
 
 }

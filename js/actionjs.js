3633edfd0857efa6ca798334f730accd42a3cf80
KaiyuanZhao <dogin2006@hotmail.com>
2015-07-15 13:55:40 +0800

    修改actionjs 上传文件同时上传注册信息

--- js/actionjs.js
+++ js/actionjs.js
@@ -1,11 +1,11 @@
 /**
  * Created by EdwardChor on 7/15/15.
  */
-function login_submit(){
-    lemail=$("#lemail").val();
-    lpassword=$("#lpassword").val();
+function login_submit() {
+    lemail = $("#lemail").val();
+    lpassword = $("#lpassword").val();
 
-    $.post("submit/submit.php",{username:lemail,password:lpassword},function(data,status){
+    $.post("submit/submit.php", {username: lemail, password: lpassword}, function (data, status) {
         alert("Data: " + data + "\nStatus: " + status);
     });
 }
@@ -18,30 +18,29 @@ function signup_submit() {
     slocation = $("#slocation").val();
     staste = $("#staste").val();
 
-
-    $.post("stest.php",{email:semail,username:susername,taste:staste,password:spassword,location:slocation},function(data,status){
-     alert("Data: " + data + "\nStatus: " + status);
-     });
-
-
-        alert("test");
-        $.ajaxFileUpload
-        (
+    $.ajaxFileUpload
+    (
+        {
+            url: 'upload.php', //用于文件上传的服务器端请求地址
+            secureuri: false, //是否需要安全协议，一般设置为false
+            fileElementId: 'savator', //文件上传域的ID
+            dataType: 'text', //返回值类型 一般设置为json
+            data: {
+                email: semail,
+                username: susername,
+                taste: staste,
+                password: spassword,
+                location: slocation},
+            success: function (data, status)  //服务器成功响应处理函数
             {
-                url: 'upload.php', //用于文件上传的服务器端请求地址
-                secureuri: false, //是否需要安全协议，一般设置为false
-                fileElementId: 'savator', //文件上传域的ID
-                dataType: 'text', //返回值类型 一般设置为json
-                success: function (data, status)  //服务器成功响应处理函数
-                {
-                    alert(data);
-                },
-                error: function (data, status, e)//服务器响应失败处理函数
-                {
-                    alert(e);
-                }
+                alert(data);
+            },
+            error: function (data, status, e)//服务器响应失败处理函数
+            {
+                alert(e);
             }
-        );
+        }
+    );
 
 
 }

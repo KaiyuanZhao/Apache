ab3cbc9229c60431e297fc753932087c6bd76269
EdwardChor <13307130149@fudan.edu.cn>
2015-07-15 21:16:42 +0800

    修改了前端用户界面的基本布局，只是中间过程。

--- js/actionjs.js
+++ js/actionjs.js
@@ -37,28 +37,72 @@ function signup_submit() {
             },
             error: function (data, status, e)//服务器响应失败处理函数
             {
-<<<<<<< HEAD
-                url: 'upload.php', //用于文件上传的服务器端请求地址
-                secureuri: false, //是否需要安全协议，一般设置为false
-                fileElementId: 'savator', //文件上传域的ID
-                data:{
-
-                }
-                dataType: 'text', //返回值类型 一般设置为json
-                success: function (data, status)  //服务器成功响应处理函数
-                {
-                    alert(data);
-                },
-                error: function (data, status, e)//服务器响应失败处理函数
-                {
-                    alert(e);
-                }
-=======
                 alert(e);
->>>>>>> 3633edfd0857efa6ca798334f730accd42a3cf80
             }
         }
     );
 
 
 }
+
+
+
+$(function() {
+    $('#doc-vld-msg').validator({
+        onValid: function(validity) {
+            $(validity.field).closest('.am-form-group').find('.am-alert').hide();
+        },
+
+        onInValid: function(validity) {
+            var $field = $(validity.field);
+            var $group = $field.closest('.am-form-group');
+            var $alert = $group.find('.am-alert');
+            var msg = $field.data('validationMessage') || this.getValidationMessage(validity);
+
+            if (!$alert.length) {
+                $alert = $('<div class="am-alert am-alert-danger"></div>').hide().
+                    appendTo($group);
+            }
+
+            $alert.html(msg).show();
+        }
+    });
+});
+
+$(document).ready(function () {
+    $('.popup-with-zoom-anim').magnificPopup({
+        type: 'inline',
+        fixedContentPos: false,
+        fixedBgPos: true,
+        overflowY: 'auto',
+        closeBtnInside: true,
+        preloader: false,
+        midClick: true,
+        removalDelay: 300,
+        mainClass: 'my-mfp-zoom-in'
+    });
+
+});
+
+
+var $ = jQuery.noConflict();
+$(function () {
+    $('#activator').click(function () {
+        $('#box').animate({'top': '0px'}, 500);
+    });
+    $('#boxclose').click(function () {
+        $('#box').animate({'top': '-700px'}, 500);
+    });
+});
+$(document).ready(function () {
+
+    //Hide (Collapse) the toggle containers on load
+    $(".toggle_container").hide();
+
+    //Switch the "Open" and "Close" state per click then slide up/down (depending on open/close state)
+    $(".trigger").click(function () {
+        $(this).toggleClass("active").next().slideToggle("slow");
+        return false; //Prevent the browser jump to the link anchor
+    });
+
+});

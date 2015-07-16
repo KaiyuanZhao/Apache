99bdc37419d30a263c97d4fcf72737dc73f10c8b
EdwardChor <13307130149@fudan.edu.cn>
2015-07-16 12:54:13 +0800

    修正了usertest.php

--- usertest.php
+++ usertest.php
@@ -1,11 +1,6 @@
 <?php
 require_once "entity/User.php";
 session_start();
-<<<<<<< HEAD
-
-$_SESSION["user"];
-=======
->>>>>>> 363e489624ce8ac3c660cc79da682ebdd1d0551a
 if(isset($_SESSION["user"])){
     echo $_SESSION["user"]->email;
 }

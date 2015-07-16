0e498f95a565d9c6557510d5e26fc0743846a68a
lyn <tongji_lyn@163.com>
2015-07-16 16:04:53 +0800

    add edit and change password

---
+++ submit/changePassword.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lyn
+ * Date: 15/7/16
+ * Time: 下午3:57
+ */
+
+    header("Content-Type: text/html; charset=utf-8");
+    date_default_timezone_set("PRC");
+    require_once "../entity/User.php";
+    require_once "../config.php";
+    require_once "../provider/Database.php";
+    require_once "../action/UserAction.php";
+    require_once "../provider/testFormat.php";
+    require_once "../entity/response/Response.php";
+    session_start();
+    $arr = $_POST;
+    $result = testChangePwd($arr);
+    $myjson = my_json_encode($result);
+    echo $myjson;
+
+    function testChangePwd($arr){
+        $userId = $_SESSION['user']->userId;
+        $previousPassword = $arr['prepassword'];
+        $newPassword = $arr['newPassword'];
+        $changeFlag = UserAction::changePassword($userId,$previousPassword,$newPassword);
+        if ($changeFlag === UserAction::$CHANGE_PASSWORD_FAIL) {
+            $result = new Response(false, "更改密码失败！");
+            return $changeFlag;
+        }
+        elseif ($changeFlag === UserAction::$CHANGE_PASSWORD_NO_USER){
+            $result = new Response(false,"没有此用户");
+            return $result;
+        }
+        elseif ($changeFlag === UserAction::$CHANGE_PASSWORD_PREVIOUS_PASSWORD_WRONG){
+            $result = new Response(false, "密码有错");
+            return $result;
+        }
+        elseif (isset($changeFlag)){
+            $result = new Response(true);
+            return $result;
+        }
+    }
+
+    function my_json_encode($phparr)
+    {
+        return json_encode($phparr);
+    }
